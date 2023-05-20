e8f949bab44ab5e7615b8d742046263cdbcee0ea
QChown <132530931+QChown@users.noreply.github.com>
2023-05-20 09:24:02 -0700

    Fixed local storage issue
    SetTodo function would clear the stored todos. Fixed by setting the initialTodos as the local storage data if available

--- src/context/TodoContext.jsx
+++ src/context/TodoContext.jsx
@@ -5,20 +5,10 @@ export const TodoContext = createContext();
 
 // Create a TodoProvider component to wrap your app
 export const TodoProvider = ({ children }) => {
-  const [todos, setTodos] = useState([]);
+  const storedTodos = localStorage.getItem("todos");
+  const initialTodos = storedTodos ? JSON.parse(storedTodos) : [];
 
-  // Load todos from localStorage on initial render
-  useEffect(() => {
-    const storedTodos = localStorage.getItem("todos");
-    if (storedTodos) {
-      setTodos(JSON.parse(storedTodos));
-    }
-  }, []);
-
-  // Update localStorage whenever todos change
-  useEffect(() => {
-    localStorage.setItem("todos", JSON.stringify(todos));
-  }, [todos]);
+  const [todos, setTodos] = useState(initialTodos);
 
   const addTodo = (newTodo) => {
     setTodos([...todos, newTodo]);
@@ -27,6 +17,10 @@ export const TodoProvider = ({ children }) => {
   const removeTodo = (todoId) => {
     setTodos(todos.filter((todo) => todo.id !== todoId));
   };
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   const contextValues = {
     todos,
